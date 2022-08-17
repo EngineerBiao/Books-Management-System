7d5a39e793d3440328874f34fd64d9f7bd11dd97
longyanbiao <2653701991@qq.com>
2022-08-17 10:37:26 +0800

    books-manegement-system v1.0

--- file.c
+++ file.c
@@ -16,10 +16,17 @@ void SaveFile(const char * file, LinkList L)  // 保存到文件(每次修改链表时就调用
 void ReadFile(const char * file, LinkList L)  // 读取文件
 {
 	FILE *fp;
-	fp = fopen(file, "r");  // 文件不存在会返回0
-	if (fp == NULL || fp == 0)
+	fp = fopen(file, "r");
+	if (fp == NULL) 
 		fp = fopen(file, "w+");  // 创建文件 
-	LinkList node = L->next;
-	while (fscanf(fp, "%d%s%lf", &node->number, node->name, &node->price) != EOF && fscanf(fp, "%d%s%lf", &node->number, node->name, &node->price) != 0) // 读到文件结尾会返回EOF 
+	LinkList node = (LinkList)malloc(sizeof(Node));
+	node->next = NULL;
+	while (fscanf(fp, "%d%s%lf", &node->number, node->name, &node->price) == 3) // 读到文件结尾会返回EOF 
+	{
 		InsertBook(L, node);
-} 
+		node = (LinkList)malloc(sizeof(Node));  // 每次插入新的都需要创建内存 
+		node->next = NULL;
+	}	
+	free(node);  // 最后会多出来一次内存分配 
+	fclose(fp);
+}
